f651c3df09fcc8eb932da5cbe20cd078c4aa7d56
mfosato <54608106+mfosato@users.noreply.github.com>
2024-02-04 12:49:18 -0700

    update

--- lib/app/modules/chatbot/views/chatbot_view.dart
+++ lib/app/modules/chatbot/views/chatbot_view.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class ChatBot extends StatefulWidget {
   const ChatBot({super.key});
@@ -50,115 +50,98 @@ class _MyAppState extends State<ChatBot> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(children: <Widget>[
-      Expanded(
-        child: Stack(
-          children: [
-            InAppWebView(
-              key: webViewKey,
-              initialUrlRequest:
-                  URLRequest(url: WebUri("https://call-gari.web.app/")),
-              initialSettings: settings,
-              pullToRefreshController: pullToRefreshController,
-              onWebViewCreated: (controller) {
-                webViewController = controller;
-              },
-              onLoadStart: (controller, url) {
-                setState(() {
-                  this.url = url.toString();
-                  urlController.text = this.url;
-                });
-              },
-              onPermissionRequest: (controller, request) async {
-                return PermissionResponse(
-                    resources: request.resources,
-                    action: PermissionResponseAction.GRANT);
-              },
-              shouldOverrideUrlLoading: (controller, navigationAction) async {
-                var uri = navigationAction.request.url!;
-
-                if (![
-                  "http",
-                  "https",
-                  "file",
-                  "chrome",
-                  "data",
-                  "javascript",
-                  "about"
-                ].contains(uri.scheme)) {
-                  if (await canLaunchUrl(uri)) {
-                    // Launch the App
-                    await launchUrl(
-                      uri,
-                    );
-                    // and cancel the request
-                    return NavigationActionPolicy.CANCEL;
-                  }
-                }
-
-                return NavigationActionPolicy.ALLOW;
-              },
-              onLoadStop: (controller, url) async {
-                pullToRefreshController?.endRefreshing();
-                setState(() {
-                  this.url = url.toString();
-                  urlController.text = this.url;
-                });
-              },
-              onReceivedError: (controller, request, error) {
-                pullToRefreshController?.endRefreshing();
-              },
-              onProgressChanged: (controller, progress) {
-                if (progress == 100) {
-                  pullToRefreshController?.endRefreshing();
-                }
-                setState(() {
-                  this.progress = progress / 100;
-                  urlController.text = url;
-                });
-              },
-              onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                setState(() {
-                  this.url = url.toString();
-                  urlController.text = this.url;
-                });
-              },
-              onConsoleMessage: (controller, consoleMessage) {
-                if (kDebugMode) {
-                  print(consoleMessage);
-                }
-              },
+    return SafeArea(
+      child: Scaffold(
+          body: Column(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Expanded(
+            child: Stack(
+              children: [
+                InAppWebView(
+                  key: webViewKey,
+                  initialUrlRequest:
+                      URLRequest(url: WebUri("https://call-gari.web.app/")),
+                  initialSettings: settings,
+                  pullToRefreshController: pullToRefreshController,
+                  onWebViewCreated: (controller) {
+                    webViewController = controller;
+                  },
+                  onLoadStart: (controller, url) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onPermissionRequest: (controller, request) async {
+                    return PermissionResponse(
+                        resources: request.resources,
+                        action: PermissionResponseAction.GRANT);
+                  },
+                  shouldOverrideUrlLoading: (controller, navigationAction) async {
+                    var uri = navigationAction.request.url!;
+                
+                    if (![
+                      "http",
+                      "https",
+                      "file",
+                      "chrome",
+                      "data",
+                      "javascript",
+                      "about"
+                    ].contains(uri.scheme)) {
+                      if (await canLaunchUrl(uri)) {
+                        // Launch the App
+                        await launchUrl(
+                          uri,
+                        );
+                        // and cancel the request
+                        return NavigationActionPolicy.CANCEL;
+                      }
+                    }
+                
+                    return NavigationActionPolicy.ALLOW;
+                  },
+                  onLoadStop: (controller, url) async {
+                    pullToRefreshController?.endRefreshing();
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onReceivedError: (controller, request, error) {
+                    pullToRefreshController?.endRefreshing();
+                  },
+                  onProgressChanged: (controller, progress) {
+                    if (progress == 100) {
+                      pullToRefreshController?.endRefreshing();
+                    }
+                    setState(() {
+                      this.progress = progress / 100;
+                      urlController.text = url;
+                    });
+                  },
+                  onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onConsoleMessage: (controller, consoleMessage) {
+                    if (kDebugMode) {
+                      print(consoleMessage);
+                    }
+                  },
+                ),
+                progress < 1.0
+                    ? LinearProgressIndicator(value: progress)
+                    : Container(),
+              ],
             ),
-            progress < 1.0
-                ? LinearProgressIndicator(value: progress)
-                : Container(),
-          ],
+          ),
         ),
-      ),
-      ButtonBar(
-        alignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            child: const Icon(Icons.arrow_back),
-            onPressed: () {
-              webViewController?.goBack();
-            },
-          ),
-          ElevatedButton(
-            child: const Icon(Icons.arrow_forward),
-            onPressed: () {
-              webViewController?.goForward();
-            },
-          ),
-          ElevatedButton(
-            child: const Icon(Icons.refresh),
-            onPressed: () {
-              webViewController?.reload();
-            },
-          ),
-        ],
-      ),
-    ]));
+       
+      ])),
+    );
   }
 }
--- lib/app/modules/scan/views/scan_view.dart
+++ lib/app/modules/scan/views/scan_view.dart
@@ -1,12 +1,13 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:yyc_scan/app/global_widgets/scaffold/scaffold.widget.dart';
 
 import '../controllers/scan_controller.dart';
+
+
+
 
 class ScanView extends GetView<ScanController> {
   const ScanView({Key? key}) : super(key: key);
@@ -23,12 +24,9 @@ class ScanView extends GetView<ScanController> {
                 width: MediaQuery.of(context).size.width * 0.6,
                 child: MobileScanner(
                   // fit: BoxFit.contain,
-                  onDetect: (capture) {
-                    final List<Barcode> barcodes = capture.barcodes;
-                    final Uint8List? image = capture.image;
-                    for (final barcode in barcodes) {
-                      debugPrint('Barcode found! ${barcode.rawValue}');
-                    }
+                  onDetect: (capture) async {
+                    EasyLoading.show(status: 'You have successfully scanned Olympic Park! You have gained 10 points');
+await Future.delayed(Duration(seconds: 6));
                   },
                 ),
               ),
